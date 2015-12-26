97049b2cf0d9a158f8eb05af2b560a89ff8ff7c9
Emily Lu <emilydlu@Emilys-MacBook-Pro-2.local>
2015-12-26 14:30:43 -0500

    added notifications

---
+++ background.js
@@ -0,0 +1,28 @@
+
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  //Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "shoot_notification" ) {
+    	chrome.alarms.create("notifier", {delayInMinutes: 0.1, periodInMinutes:0.1});
+    	chrome.alarms.onAlarm.addListener(function(notifier){
+          chrome.notifications.create(
+          	'id1', 
+          	{type: "basic", 
+          	title: "whatever",
+          	message: "It's been 3 hours asshole, look at your life",
+          	iconUrl: "Netflix.jpg"
+          }
+   )
+        })
+  }
+}
+);
